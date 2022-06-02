055defa1d12aea92706acf5bbd73ee9bffb6bb45
Jim Sheldon <jim.sheldon@gmail.com>
2022-06-02 16:01:05 -0400

    use provider file for both tests

--- tests/simple/test.tf
+++ tests/simple/test.tf
@@ -12,43 +12,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  access_key = "test"
-  secret_key = "test"
-  region     = "us-east-2"
-
-  s3_use_path_style           = false
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    apigateway     = "http://localstack:4566"
-    apigatewayv2   = "http://localstack:4566"
-    cloudformation = "http://localstack:4566"
-    cloudwatch     = "http://localstack:4566"
-    dynamodb       = "http://localstack:4566"
-    ec2            = "http://localstack:4566"
-    es             = "http://localstack:4566"
-    elasticache    = "http://localstack:4566"
-    firehose       = "http://localstack:4566"
-    iam            = "http://localstack:4566"
-    kinesis        = "http://localstack:4566"
-    lambda         = "http://localstack:4566"
-    rds            = "http://localstack:4566"
-    redshift       = "http://localstack:4566"
-    route53        = "http://localstack:4566"
-    s3             = "http://localstack:4566"
-    secretsmanager = "http://localstack:4566"
-    ses            = "http://localstack:4566"
-    sns            = "http://localstack:4566"
-    sqs            = "http://localstack:4566"
-    ssm            = "http://localstack:4566"
-    stepfunctions  = "http://localstack:4566"
-    sts            = "http://localstack:4566"
-  }
-}
-
 module "main" {
   # source is always ../.. for test suite configurations,
   # because they are placed two subdirectories deep under
